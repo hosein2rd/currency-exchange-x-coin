402d3234d24b6a0708de802cd9c83409cd73b76c
hossein <mdoroud97@gmail.com>
2023-05-14 23:35:28 +0330

    feat: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,15 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => (
+    <input
+      className={`border-b-2 border-dashed border-black p-2 ${className}`}
+      ref={ref}
+      {...props}
+    />
+  )
+);
+
+export default Input;
