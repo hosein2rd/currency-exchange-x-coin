1f5befb703d93cee88bc39d8d109a2a97c913619
hossein <mdoroud97@gmail.com>
2023-05-14 23:37:23 +0330

    fix: use forwardRef for button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,17 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, ...props }: ButtonProps) => (
-  <button
-    className="self-start bg-pink-600 py-1.5 px-3 text-white rounded-md hover:bg-pink-700 transition-all active:shadow-2xl active:shadow-gray-600"
-    {...props}
-  >
-    {children}
-  </button>
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, className = "", ...props }, ref) => (
+    <button
+      ref={ref}
+      className={`self-start bg-pink-600 py-1.5 px-3 text-white rounded-md hover:bg-pink-700 transition-all active:shadow-2xl active:shadow-gray-600 ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
 );
 
 export default Button;
