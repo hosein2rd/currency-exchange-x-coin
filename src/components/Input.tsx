39c8ae371e8a27dfc2699e8f6feef113684b5a9d
hossein <mdoroud97@gmail.com>
2023-05-15 23:50:07 +0330

    fix: remove outline of input while focus

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ type InputProps = InputHTMLAttributes<HTMLInputElement>;
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className = "", ...props }, ref) => (
     <input
-      className={`border-b-2 border-dashed border-black p-2 ${className}`}
+      className={`border-b-2 border-dashed border-black p-2 outline-none ${className}`}
       ref={ref}
       {...props}
     />
