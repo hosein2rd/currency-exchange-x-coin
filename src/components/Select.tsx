d0ad1c5813ec91d2a8eb999643411fe86bffa2e7
hossein <mdoroud97@gmail.com>
2023-05-15 23:33:00 +0330

    feat: add Select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,18 @@
+import { SelectHTMLAttributes, forwardRef } from "react";
+
+export type Option = { title: string; value: string };
+type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
+  options: Option[];
+};
+
+const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ className = "", options = [], ...props }, ref) => (
+    <select ref={ref} className={`${className}`} {...props}>
+      {options.map(({ value, title }) => (
+        <option value={value}>{title}</option>
+      ))}
+    </select>
+  )
+);
+
+export default Select;
