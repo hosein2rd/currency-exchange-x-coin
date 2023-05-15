4e26d45e324717b5d4aada28e79059ca2e2e6f05
hossein <mdoroud97@gmail.com>
2023-05-15 23:32:35 +0330

    feat: add constant currency

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+import { Option } from "../components/Select";
+
+export const currencies: Option[] = [
+  { title: "USD", value: "USD" },
+  { title: "GBP", value: "GBP" },
+  { title: "EUR", value: "EUR" },
+];
