b6963940b98c0dd405ca134653e089251fe03def
hossein <mdoroud97@gmail.com>
2023-05-16 00:28:14 +0330

    feat: inner shadow for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, className = "", ...props }, ref) => (
     <button
       ref={ref}
-      className={`self-start bg-pink-600 py-1.5 px-3 text-white rounded-md hover:bg-pink-700 transition-all active:shadow-2xl active:shadow-gray-600 ${className}`}
+      className={`self-start bg-pink-600 py-1.5 px-3 text-white rounded-md hover:bg-pink-700 transition-all active:shadow-inner active:shadow-pink-900 ${className}`}
       {...props}
     >
       {children}
