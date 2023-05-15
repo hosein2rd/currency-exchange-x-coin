8d7b59f055d9c904ce0d86ea24bd115ecdacdc58
hossein <mdoroud97@gmail.com>
2023-05-16 00:06:47 +0330

    feat: add ExchageRows to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Button from "./components/Button";
+import ExchangeRow from "./components/ExchangeRow";
 
 function App() {
   return (
     <div className="flex flex-col w-full">
+      <div className="relative flex flex-col">
+        <ExchangeRow addingMode />
+        <ExchangeRow addingMode={false} />
+      </div>
       <Button>Exchange</Button>
     </div>
   );
