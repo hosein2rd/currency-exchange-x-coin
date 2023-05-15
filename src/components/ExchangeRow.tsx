342fee5c1df3320134688ac09ab2d179d4ebf877
hossein <mdoroud97@gmail.com>
2023-05-16 00:06:25 +0330

    feat: add ExchageRow component

---
+++ src/components/ExchangeRow.tsx
@@ -0,0 +1,27 @@
+import { currencies } from "../constants";
+import Balance from "./Balance";
+import Input from "./Input";
+import Select from "./Select";
+
+type ExchangeRowProps = { addingMode: boolean };
+
+const ExchangeRow = ({ addingMode = false }: ExchangeRowProps) => {
+  return (
+    <div
+      className={`flex flex-row justify-between p-5 ${
+        !addingMode ? "bg-gray-100" : ""
+      }`}
+    >
+      <div className="flex flex-col">
+        <Select className="self-baseline" options={currencies} />
+        <Balance value={200} currency="$" />
+      </div>
+      <div className="flex flex-row items-center">
+        <span className="text-2xl">{addingMode ? "+" : "-"}</span>
+        <Input type="number" className="w-32" />
+      </div>
+    </div>
+  );
+};
+
+export default ExchangeRow;
