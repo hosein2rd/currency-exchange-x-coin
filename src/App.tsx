dbb591a73fa41510b65a6bfbcd949d83e698e9b7
hossein <mdoroud97@gmail.com>
2023-05-16 00:29:36 +0330

    feat: add wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import CurrencyExchage from "./components/CurrencyExchage";
 
 function App() {
   return (
-    <div className="flex flex-col w-full">
-      <div className="relative flex flex-col">
-        <ExchangeRow addingMode />
-        <CurrencyExchage
-          className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
-          text="1$ = 120$"
-        />
-        <ExchangeRow addingMode={false} />
+    <div className="w-screen h-screen bg-gray-200 flex justify-center items-center">
+      <div className="flex flex-col w-2/5 bg-white p-4 rounded-md shadow-xl">
+        <div className="relative flex flex-col">
+          <ExchangeRow addingMode />
+          <CurrencyExchage
+            className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+            text="1$ = 120$"
+          />
+          <ExchangeRow addingMode={false} />
+        </div>
+        <Button className="mt-4">Exchange</Button>
       </div>
-      <Button>Exchange</Button>
     </div>
   );
 }
