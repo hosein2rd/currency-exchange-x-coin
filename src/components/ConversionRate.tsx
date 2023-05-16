e305707816b8fb3c96c99185935ef3a417578150
hossein <mdoroud97@gmail.com>
2023-05-16 15:03:09 +0330

    fix: width of ConversionRate

--- src/components/ConversionRate.tsx
+++ src/components/ConversionRate.tsx
@@ -2,7 +2,7 @@ type ConversionRateProps = { className?: string; text: string };
 
 const ConversionRate = ({ className = "", text }: ConversionRateProps) => (
   <span
-    className={`bg-white border-green-700 w-fit rounded-full border-2 px-3 py-0.5 ${className}`}
+    className={`bg-white border-green-700 w-max rounded-full border-2 px-3 py-0.5 ${className}`}
   >
     {text}
   </span>
