29ef2bf998645cfef8ac1aafc37a50dcce2b94d1
hossein <mdoroud97@gmail.com>
2023-05-17 01:05:12 +0330

    feat: add disabled style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, className = "", ...props }, ref) => (
     <button
       ref={ref}
-      className={`self-start bg-pink-600 py-1.5 px-3 text-white rounded-md hover:bg-pink-700 transition-all active:shadow-inner active:shadow-pink-900 ${className}`}
+      className={`self-start bg-pink-600 py-1.5 px-3 text-white rounded-md hover:bg-pink-700 transition-all active:shadow-inner active:shadow-pink-900 ${className} disabled:bg-gray-500 disabled:cursor-not-allowed`}
       {...props}
     >
       {children}
