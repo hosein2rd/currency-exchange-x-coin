5b68180a6432e1e550bc7ea1f84a48371e22e328
hossein <mdoroud97@gmail.com>
2023-05-16 14:17:11 +0330

    fix: change onSelect for Select component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,13 +3,21 @@ import { SelectHTMLAttributes, forwardRef } from "react";
 export type Option = { title: string; value: string };
 type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
   options: Option[];
+  onSelect: (value: string) => void;
 };
 
 const Select = forwardRef<HTMLSelectElement, SelectProps>(
-  ({ className = "", options = [], ...props }, ref) => (
-    <select ref={ref} className={`bg-inherit min-w-0 ${className}`} {...props}>
-      {options.map(({ value, title }) => (
-        <option value={value}>{title}</option>
+  ({ className = "", options = [], onSelect, ...props }, ref) => (
+    <select
+      onChange={(e) => onSelect(e.currentTarget.value)}
+      ref={ref}
+      className={`bg-inherit min-w-0 ${className}`}
+      {...props}
+    >
+      {options.map(({ value, title }, i) => (
+        <option key={i} value={value}>
+          {title}
+        </option>
       ))}
     </select>
   )
