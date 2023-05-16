73a3e0dd14e336dc3d83896f79b87debad0efbd3
hossein <mdoroud97@gmail.com>
2023-05-16 13:31:05 +0330

    refactor: rename CurrencyExchange to ConversionRate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./components/Button";
 import ExchangeRow from "./components/ExchangeRow";
-import CurrencyExchage from "./components/CurrencyExchage";
+import ConversionRate from "./components/ConversionRate";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <div className="flex flex-col w-2/5 bg-white p-4 rounded-md shadow-xl">
         <div className="relative flex flex-col">
           <ExchangeRow addingMode />
-          <CurrencyExchage
+          <ConversionRate
             className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
             text="1$ = 120$"
           />
---
+++ src/components/ConversionRate.tsx
@@ -0,0 +1,11 @@
+type ConversionRateProps = { className?: string; text: string };
+
+const ConversionRate = ({ className = "", text }: ConversionRateProps) => (
+  <span
+    className={`bg-white border-green-700 w-fit rounded-full border-2 px-3 py-0.5 ${className}`}
+  >
+    {text}
+  </span>
+);
+
+export default ConversionRate;
